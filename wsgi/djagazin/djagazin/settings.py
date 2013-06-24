2b30b2443fe98aabbf1184f4f75a1fff01e3abf0
Xuan-Thuy Dang <ThuyDang.de@gmail.com>
2013-06-24 14:38:19 +0200

    --

--- wsgi/djagazin/djagazin/settings.py
+++ wsgi/djagazin/djagazin/settings.py
@@ -28,14 +28,26 @@ if ON_OPENSHIFT:
     # with rhc-ctl-app (see /README in this git repo)
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': os.environ['OPENSHIFT_APP_NAME'], # Or path to database file if using sqlite3.
-            'USER': os.environ['OPENSHIFT_DB_USERNAME'], # Not used with sqlite3.
-            'PASSWORD': os.environ['OPENSHIFT_DB_PASSWORD'], # Not used with sqlite3.
-            'HOST': os.environ['OPENSHIFT_DB_HOST'], # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': os.environ['OPENSHIFT_DB_PORT'], # Set to empty string for default. Not used with sqlite3.
+            #'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            #'NAME': os.environ['OPENSHIFT_APP_NAME'], # Or path to database file if using sqlite3.
+            #'USER': os.environ['OPENSHIFT_DB_USERNAME'], # Not used with sqlite3.
+            #'PASSWORD': os.environ['OPENSHIFT_DB_PASSWORD'], # Not used with sqlite3.
+            #'HOST': os.environ['OPENSHIFT_DB_HOST'], # Set to empty string for localhost. Not used with sqlite3.
+            #'PORT': os.environ['OPENSHIFT_DB_PORT'], # Set to empty string for default. Not used with sqlite3.
+						'ENGINE': 'django.db.backends.sqlite3',
+						'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),
+						'USER': '',
+						'PASSWORD': '',
+						'HOST': '',
+						'PORT': '',
         }
     }
+
+		connect('djagazin',
+            username=os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
+            password=os.environ['OPENSHIFT_MONGODB_DB_PASSWORD'],
+            host=os.environ['OPENSHIFT_MONGODB_DB_HOST'],
+            port=int(os.environ['OPENSHIFT_MONGODB_DB_PORT']))
 else:
 	# Use this for your local settings.
 
