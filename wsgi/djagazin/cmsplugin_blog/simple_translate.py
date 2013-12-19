5e5d4bed77a190b3276826ee0e1b3160e083d03c
Xuan-Thuy Dang <ThuyDang.de@gmail.com>
2013-12-19 19:48:18 +0100

    delete old version

--- wsgi/djagazin/cmsplugin_blog/simple_translate.py
+++
@@ -1,5 +0,0 @@
-from cmsplugin_blog.models import Entry, EntryTitle
-from simple_translation.translation_pool import translation_pool
-
-translation_pool.register_translation(Entry, EntryTitle)
-
--- wsgi/djagazin/djagazin/themes/wp_clear3/static/js/sprinkle-tabs.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery('#solostream-settings > ul').tabs();
-	jQuery('#widget-tabs > ul').tabs();
-	jQuery('#archive-tabs > ul').tabs();
-	jQuery('#tabbed-cat > ul').tabs();
-});
